5731f6c8d7294ce97e91acad1fa683e234a5f6d9
Dan Henry <s3543854@student.rmit.edu.au>
2020-05-06 22:04:19 +1000

    map init

--- static/map.js
+++ static/map.js
@@ -1,8 +1,8 @@
-var map;
+mapboxgl.accessToken = 'pk.eyJ1IjoiZGhlbnJ5NDM3IiwiYSI6ImNrOXY3cjUxNjA5cDQzZHBrc2w1N3M4em4ifQ.hzae72u-br3ad-rYz_2h2g';
 
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: -37.8136, lng: 144.9631 },
-        zoom: 12
-    });
-}
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11', //'mapbox://styles/dhenry437/ck9v860rb00dp1inpfefyxgww'
+    center: { lat: -37.8136, lng: 144.9631 },
+    zoom: 12
+});
