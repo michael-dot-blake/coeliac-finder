8711f0abf466057638e25c5d4dd343b84b75b9ab
Dan Henry <s3543854@student.rmit.edu.au>
2020-05-06 20:41:01 +1000

    initial commit

--- static/map.js
+++ static/map.js
@@ -1,8 +1,14 @@
-var map;
+var platform = new H.service.Platform({
+    'apikey': 'u7jeE0PSCqmSBID1ynopfhCCE0626iLwV2aV4q1R_Rg'
+});
 
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
+// Obtain the default map types from the platform object:
+var defaultLayers = platform.createDefaultLayers();
+
+// Instantiate (and display) a map object:
+var map = new H.Map(
+    document.getElementById('mapContainer'),
+    defaultLayers.vector.normal.map, {
         center: { lat: -37.8136, lng: 144.9631 },
         zoom: 12
     });
-}
